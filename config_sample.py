ae8963c0e0c76fea84526081b415f3d5a1f07d5c
Ashutosh15rai <115929606+Ashutosh15rai@users.noreply.github.com>
2025-04-11 22:43:12 +0530

    Update config_sample.py

--- config_sample.py
+++ config_sample.py
@@ -81,7 +81,7 @@ USENET_SERVERS = [
 ]
 
 # Update
-UPSTREAM_REPO = "https://github.com/AeonOrg/Aeon-MLTB"
+UPSTREAM_REPO = "https://github.com/Ashutosh15rai/nl"
 UPSTREAM_BRANCH = "main"
 
 # Leech
